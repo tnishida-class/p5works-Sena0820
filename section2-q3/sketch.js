694fa1657e79f64b78069c2164c3b3ad370dac14
Fukui0820 <as3104523@gmail.com>
2020-10-20 22:42:54 +0900

    q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -13,11 +13,16 @@ function setup() {
 
   cx = width / 2;
   cy = height / 2;
-  maxR = min(width, height);
+  maxR = min(width, height); //横幅と高さの小さいほうを返す
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+
+  drawArcs(cream, black,maxR * 0.75);
+  drawArcs(green, red,maxR * 0.5);
+  drawArcs(cream, black,maxR * 0.45);
+  drawCircle(green, maxR * 0.1);
   drawCircle(red, maxR * 0.05);
 }
 
@@ -25,12 +30,13 @@ function drawCircle(c, r){
   fill(c);
   ellipse(cx, cy, r, r);
 }
+// これは先生の自作関数
 
 function drawArcs(c1, c2, r) {
   for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
+    let start = TWO_PI / 20 * i;//PIは円周率での二倍
     let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
+    fill(i % 2 == 0 ? c1 : c2);//c1,c2のいずれかを条件で選ぶ
     arc(cx, cy, r, r, start, stop, PIE);
   }
 }
