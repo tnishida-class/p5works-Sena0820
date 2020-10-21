903dbe2e252d9ea64dd629484f038e501e0c98ca
Fukui0820 <as3104523@gmail.com>
2020-10-21 19:24:27 +0900

    update-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -8,8 +8,19 @@ function setup() {
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
+
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
+    if(i % 2 !== 0) {
+      fill(255);
+      rect(0,i * d,width,height / 9);
+
+    } else {
+      fill(blue);
+      rect(0,i * d,width,height / 9);
+    }
+
+
   }
 
   fill(blue);
@@ -18,4 +29,8 @@ function setup() {
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(2 * d,0,d,5 * d);
+  fill(255);
+  noStroke();
+  rect(0,2 * d,5 * d,d);
 }
