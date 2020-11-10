d6640e0f965cf9e2169907b66e81cb99b7cee7bc
Fukui0820 <as3104523@gmail.com>
2020-11-10 10:43:15 +0900

    Update2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,25 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100);
+  background(196);
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+
+    // if(i < 5) {
+      let x = i * 10 + 10
+
+      // fill(0,0,255);
+      stroke(255,0,0);
+      noFill();
+      ellipse(50,50, x);
+      if(i < 5) {
+        stroke(0,0,255);
+        ellipse(50,50,x);
+      }
+
+
+
+    // }
+
   }
 }
