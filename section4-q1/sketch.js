3510873d70f2cfaf1a75f6f84a5e8063000ffa5a
Sena0820 <fujingho747@gmail.com>
2020-11-17 00:06:24 +0900

    updateq4

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -5,12 +5,22 @@ let cycle;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 80;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
+  let size;
+  size = count + 100;
+  // size += 1;
+  fill(199,60,46);
   ellipse(width / 2, height / 2, size);
+  // size += 100;
+
+  if (size > 160) {
+    // size += 50;
+    size = 150;
+  }
 }
--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -4,7 +4,9 @@ let x, y, vx, vy;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
+  // x = 0;
   y = height / 2;
+  // y = 0;
   vx = 8;
   vy = 8;
 }
@@ -12,7 +14,11 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-
+  noStroke();
+  fill(255,241,86)
+  star(x,y,20,90);
+  x += vx;
+  y += vy;
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -23,13 +29,22 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var rotation = rotation + 90;
+    // rotation = rotation + 100;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + rotation;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    // let rot;
+    // rot = rot + (angle / 360);
+    // theta += angle / 360;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
+
     vertex(x,y);
+
   }
+
   endShape(CLOSE);
+
 }
 
 function windowResized(){
